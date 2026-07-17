3f1f66d386217db3142b3ae984fe690ca228a211
Daniel Finnan <dan@custom-made.org.uk>
2026-07-17 09:52:45 +0200

    Tidy up.

--- api/helpers.py
+++
@@ -1,9 +0,0 @@
-import logging
-import json
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
-
-with open('config.json', mode="r") as input_file:
-    config = json.load(input_file)
-    storage = config['storage']
